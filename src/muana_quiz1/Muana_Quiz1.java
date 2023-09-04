5f718c4ea22f8ef7201f26d4bc8b95ec540f7c12
User <User@192.168.3.124>
2023-09-04 16:23:43 +0800

    version1

---
+++ src/muana_quiz1/Muana_Quiz1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package muana_quiz1;
+
+/**
+ *
+ * @author User
+ */
+public class Muana_Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
